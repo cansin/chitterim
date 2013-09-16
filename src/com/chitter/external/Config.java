8aca53c96708125e8c528bdaaae1b6bef4659323
Cansin Yildiz <cansinyildiz@gmail.com>
2013-09-15 20:51:13 -0700

    Submit a dummy Config.java

---
+++ src/com/chitter/external/Config.java
@@ -0,0 +1,22 @@
+package com.chitter.external;
+
+public class Config {
+
+	static final String bitlyApiKey = 
+			"YOUR_BITLY_API_KEY_GOES_HERE";
+	
+	static final String twChitterScreenName = 
+			"YOUR_BOTS_TWITTER_ACCOUNT_GOES_HERE";
+	
+	static final String twChitterToken =
+			"YOUR_BOTS_TWITTER_TOKEN_GOES_HERE";
+
+	static final String twChitterSecret =
+			"YOUR_BOTS_TWITTER_SECRET_GOES_HERE";
+
+	static final String twConsumerSecret =
+			"YOUR_TWITTER_APPS_CONSUMER_SECRET_GOES_HERE";
+
+	static final String twConsumerKey = 
+			"YOUR_TWITTER_APPS_CONSUMER_KEY_GOES_HERE";
+}
